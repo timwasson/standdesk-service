0e2aef975be317d241ff260fa25ec272e066c040
Tim Wasson <wasson@gmail.com>
2015-12-30 22:22:43 +0000

    Adding distance sensor

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 var express = require('express');
+var usonic = require('r-pi-usonic');
 var app = express();
 var gpio = require('gpio');
 var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
+// Establish variables for desk distance, status and timer
+var distance, sitStand, timer;
+
+// Get desk config
+var appConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+//var standDuration = parseInt(appConfig.standDuration);
+//var sitDuration = parseInt(appConfig.sitDuration);
+
+var standDuration = 120;
+var sitDuration = 100;
+
+// Initialize the distance sensor
+usonic.init(function (error) {
+  if (error) {
+    console.log(error);
+  } else {
+    var sensor = usonic.createSensor(23, 27, 1000);
+    distance = sensor();
+    setInterval(function() {
+      distance = sensor();
+    }, 500);
+    if(distance < 72) {
+      sitStand = 'sit';
+      timer = sitDuration;
+    } else {
+      sitStand = 'stand';
+      timer = standDuration;
+    }
+  }
+});
+
+// Set up jade templating engine
 app.set('view engine', 'jade');
 app.set('views', 'views');
 
+// Set up express and the bodyParser to save desk configuration
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-/* NOTE: SASS is compiled with compass outside of node.js now  */
 
 // Exporting GPIO pins
 var power = gpio.export(2, {
@@ -29,39 +62,36 @@ var down = gpio.export(4, {
 });
 // End pin export
 
-var appConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-
-var standDuration = parseInt(appConfig.sitDuration);
-var sitDuration = parseInt(appConfig.standDuration);
-
-// Set the timer at zero and force a stand
-var timer = 0;
-var sitStand = 'stand';
-
 setInterval(function(){
+  // Ensure sitting/standing is set properly by checking distance
+  if(distance < 72) {
+    sitStand = 'sit';
+  } else {
+    sitStand = 'stand';
+  }
   // Only run this on certain days and certain times
   var today = new Date();
   // Don't run on the weekends (6 and 0) or before/after certain hours (8 and 5)
-  if(today.getDay() !=6 && today.getDay() != 0 && today.getHours() >= 8 && today.getHours() <= 17) {
-    timer++;
-    console.log("time: " + timer);
-    if(sitStand == 'stand' && timer >= standDuration) {
+  //if(today.getDay() !=6 && today.getDay() != 0 && today.getHours() >= 8 && today.getHours() <= 17) {
+    timer--;
+    console.log("Time Remaining: " + timer + " | Distance: " + distance + " | Currently: " + sitStand);
+    if(sitStand == 'stand' && timer == 0) {
       down.set(1);
       up.set(0);
   
-      timer = 0;
+      timer = sitDuration;
       sitStand = 'sit';
-    } else if(sitStand == 'sit' && timer >= sitDuration) {
+    } else if(sitStand == 'sit' && timer == 0) {
       down.set(0);
       up.set(1);
       
-      timer = 0;
+      timer = standDuration;
       sitStand = 'stand';
     }
-  }
+  //}
 }, 
 // 1 minute
-60000
+1000
 );
 
 app.get('/', function (req, res) {
@@ -86,13 +116,19 @@ app.get('/up', function (req, res) {
 
 app.get('/down', function (req, res) {
   // Send back JSON
-  var foo = {sitStand:'stand'};
+  var foo = {sitStand:'sit'};
   var jsonString = JSON.stringify(foo);
   res.send(jsonString);
   
   // Set GPIO to sit
   down.set(1);
   up.set(0);
+});
+
+app.get('/status', function (req, res) {
+  var foo = {status:sitStand, timeRemain:timer,distance:distance};
+  var jsonString = JSON.stringify(foo);
+  res.send(jsonString);
 });
 
 app.post('/save',function(req,res){
